dde38650d0a0aa854f82dbd7a739d3a3a4881583
khomike <mike@qualcode.ru>
2024-02-29 21:43:32 +0300

    29/02/2024 - 21:43

---
+++ src/components/Content.vue
@@ -0,0 +1,86 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<template>
+    <div class="content">
+        <div class="exercise">
+            <div class="exercise-header">
+                <div class="exercise-header__left">
+                    <div class="exercise-title">
+                        <div class="exercise-title__name">Умножение на 2</div>
+                        <div class="exercise-title__description">описание</div>
+                    </div>
+                </div>
+                <div class="exercise-header__center">
+                    <div class="exercise-statistic">
+                        <div class="exercise-statistic__item">
+                            <div class="exercise-statistic__title">
+                                Всего ответов:
+                            </div>
+                            <div class="exercise-statistic__value">0</div>
+                        </div>
+                        <div class="exercise-statistic__item">
+                            <div class="exercise-statistic__title">Верно:</div>
+                            <div class="exercise-statistic__value">0</div>
+                        </div>
+                        <div class="exercise-statistic__item">
+                            <div class="exercise-statistic__title">Ошибок:</div>
+                            <div class="exercise-statistic__value">0</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="exercise-header__right">
+                    <div class="exercise-timer">
+                        <div class="exercise-timer__title">Таймер:</div>
+                        <div class="exercise-timer__value">00:00:00</div>
+                    </div>
+                </div>
+            </div>
+            <div class="exercise-content">
+                <div class="exercise-content__task">2 x 2 = ?</div>
+                <div class="exercise-content__answer">
+                    <div class="answer--low-level">
+                        <span
+                            class="choose-item btn"
+                            onclick="checkAnswer(this)"
+                            >23</span
+                        >
+                        <span
+                            class="choose-item btn"
+                            onclick="checkAnswer(this)"
+                            >58</span
+                        >
+                        <span
+                            class="choose-item btn"
+                            onclick="checkAnswer(this)"
+                            >75</span
+                        >
+                        <span
+                            class="choose-item btn"
+                            onclick="checkAnswer(this)"
+                            >46</span
+                        >
+                    </div>
+                    <div class="answer--hight-level">
+                        <div class="answer--hight-level__input">
+                            <input
+                                type="number"
+                                placeholder="Ответ"
+                                autofocus
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="exercise-content__btn">
+                    <input type="button" value="Ответить" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<template>
+    <div class="header">
+        <div class="hader-start">
+            <div class="siteBar">
+                <div class="site-logo header-item">
+                    <h2>MTrainer</h2>
+                </div>
+                <div class="btn button-toMain header-item">
+                    <a href="#">
+                        <div class="btn">На главную</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header-end">
+            <div class="userBar">
+                <div class="userBar-btn header-item">
+                    <div class="userBar-btn-avatar">
+                        <div class="avatar-box">
+                            <img
+                                src="./src/assets/img/avatar.jpg"
+                                height="3rem;"
+                                class="img-avatar--small"
+                                alt="avatar"
+                            />
+                        </div>
+                    </div>
+                    <div class="userBar-btn-title">Хозяинов Михаил</div>
+                </div>
+                <div class="userBar-menu header-item">
+                    <a href="http://localhost:5500/temp-login.html">
+                        <div class="button-nav button-nav-exit">Выйти</div>
+                    </a>
+                    <div class="btn">
+                        <a href="#">
+                            <div class="btn">Статистика</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
