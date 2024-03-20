f273bb9093fca3941184bcb2a99d32bdc51f0b33
Mike Kh <mv.xo@yandex.ru>
2024-03-20 15:52:13 +0300

    20/03/2024 - 15:51

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <script setup>
+@import url('./assets/fonts/Ubuntu-Bold.ttf');
+@import url('./assets/fonts/Ubuntu-Bolditalic.ttf');
+@import url('./assets/fonts/Ubuntu-Italic.ttf');
+@import url('./assets/fonts/Ubuntu-Light.ttf');
+@import url('./assets/fonts/Ubuntu-Lightitalic.ttf');
+@import url('./assets/fonts/Ubuntu-Medium.ttf');
+@import url('./assets/fonts/Ubuntu-Mediumitalic.ttf');
+@import url('./assets/fonts/Ubuntu-Regular.ttf');
+
 import Header from "./components/Header.vue";
 import Content from "./components/Content.vue";
 </script>
