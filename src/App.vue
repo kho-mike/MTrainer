565c93fd6def25361502f841350590214f2733e1
Mike Kh <mv.xo@yandex.ru>
2024-03-20 16:47:46 +0300

    20/03/2024 - 16:47

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,6 @@ import Content from "./components/Content.vue";
 </template>
 
 <style>
-@import url('./assets/fonts/Ubuntu-Bold.ttf');
-@import url('./assets/fonts/Ubuntu-Bolditalic.ttf');
-@import url('./assets/fonts/Ubuntu-Italic.ttf');
-@import url('./assets/fonts/Ubuntu-Light.ttf');
-@import url('./assets/fonts/Ubuntu-Lightitalic.ttf');
-@import url('./assets/fonts/Ubuntu-Medium.ttf');
-@import url('./assets/fonts/Ubuntu-Mediumitalic.ttf');
-@import url('./assets/fonts/Ubuntu-Regular.ttf');
+
 
 </style>
