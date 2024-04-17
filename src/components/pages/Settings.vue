4fac42aff6b4b42ad76d42cf9dc39e2d7ed1ea96
Mike Kh <mv.xo@yandex.ru>
2024-04-17 16:08:50 +0300

    17/04/2024 - 16:07

--- src/components/pages/Settings.vue
+++ src/components/pages/Settings.vue
@@ -82,6 +82,7 @@ console.log('This is Exercise page!');
 import Button       from "@/components/elements/Button.vue";
 import ButtonNav    from "@/components/elements/ButtonNav.vue";
 import Msg          from "@/components/elements/Msg.vue";
+import Inp          from "@/components/elements/Inp.vue";
 import { useConstsStore } from "@/stores";
 import { reactive, ref } from "vue";
 
